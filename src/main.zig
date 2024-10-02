d381878ae2bb7cc0817c4945eb09f04d4c0c64d3
Thomas Aunvik <contact@thaun.dev>
2024-10-02 21:46:48 +0200

    Remove Fuzz test

--- src/main.zig
+++ src/main.zig
@@ -25,13 +25,3 @@ test "simple test" {
     try list.append(42);
     try std.testing.expectEqual(@as(i32, 42), list.pop());
 }
-
-test "fuzz example" {
-    const global = struct {
-        fn testOne(input: []const u8) anyerror!void {
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(global.testOne, .{});
-}
